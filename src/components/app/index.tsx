3e746e85a08581121cff49cba4117a8221da0839
thageesan <thageesan@gmail.com>
2020-05-25 22:24:59 -0400

    initial commit

---
+++ src/components/app/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function App() {
+    return (
+        <h1>Welcome to the Pick 3 Demo</h1>
+    )
+}
---
+++ webpack.dev.config.js
@@ -0,0 +1,78 @@
+const webpack = require('webpack');
+const fs = require('fs');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { TsconfigPathsPlugin } = require('tsconfig-paths-webpack-plugin');
+const dotenv = require('dotenv');
+
+module.exports = (env) => {
+
+    // Get the root path (assuming your webpack config is in the root of your project!)
+    const currentPath = path.join(__dirname);
+
+    // Create the fallback path (the production .env)
+    const basePath = currentPath + '/.env';
+
+    // We're concatenating the environment name to our filename to specify the correct env file!
+    const envPath = basePath + '.' + env.ENVIRONMENT;
+
+    // Check if the file exists, otherwise fall back to the production .env
+    const finalPath = fs.existsSync(envPath) ? envPath : basePath;
+
+    // Set the path parameter in the dotenv config
+    const fileEnv = dotenv.config({ path: finalPath }).parsed;
+
+    // reduce it to a nice object, the same as before (but with the variables from the file)
+    const envKeys = Object.keys(fileEnv).reduce((prev, next) => {
+        prev[`process.env.${next}`] = JSON.stringify(fileEnv[next]);
+        return prev;
+    }, {});
+
+    return {
+        context: path.resolve(__dirname),
+        mode: 'development',
+        entry: './src/index.tsx',
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    loader: "ts-loader"
+                },
+                {
+                    test: /\.less$/,
+                    use: [
+                        'style-loader',
+                        'css-loader',
+                        'less-loader',
+                    ],
+                },
+                {
+
+                    test: /\.(png|svg|jpg|gif)$/,
+                    loader: 'file-loader',
+                },
+            ]
+        },
+        resolve: {
+            extensions: ['.ts', '.js', '.tsx', '.json'],
+            alias: {
+                // To set absolute path for the application starting with SRC
+                src: path.resolve('src'),
+            },
+            plugins: [new TsconfigPathsPlugin()]
+        },
+        devServer: {
+            port: 9001
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: "./src/index.html"
+            }),
+            new HtmlWebpackPlugin({
+                template: "./src/index.html",
+                filename: "./index.html"
+            }),
+            new webpack.DefinePlugin(envKeys),
+        ],
+    }
+};
