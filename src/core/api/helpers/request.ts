1d6af9225b9f63500c1329189fc7c29c7e01c7d4
thageesan <thageesan@gmail.com>
2020-05-28 00:08:52 -0400

    - requests now includes cookies

--- src/core/api/helpers/request.ts
+++ src/core/api/helpers/request.ts
@@ -6,11 +6,11 @@ function request(address: string, method: 'GET' | 'POST' | 'PUT' | 'DELETE' = 'G
         let config = {
             method: `${method}`, // *GET, POST, PUT, DELETE, etc.
             cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-            credentials: 'same-origin', // include, *same-origin, omit
+            credentials: 'include', // include, *same-origin, omit
             headers: headers,
             redirect: 'follow', // manual, *follow, error
             referrerPolicy: 'no-referrer', // no-referrer, *client
-        }
+        };
         if (method === 'POST') {
             // @ts-ignore
             config['body'] = JSON.stringify(params) // body data type must match "Content-Type" header
